187dec4b76cf3d514664869903d3c1aab9defb25
Pushp Raj <raj.pushp05@gmail.com>
2020-06-26 12:32:31 +0530

    variable declaration and assignment

---
+++ src/variableDeclarationAndAssignment.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    
+	x := 42 // declatarion and assignment
+
+	fmt.Println(x)
+
+	x = 72 // assignment here we use only "="
+
+	fmt.Println(x)
+
+}
