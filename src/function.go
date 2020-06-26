525669d62fe962b0ce765152e8ba60068b7dbd4b
Pushp Raj <raj.pushp05@gmail.com>
2020-06-26 11:06:17 +0530

    Function calling example

---
+++ src/function.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Example for function and function calling")
+
+    mycustomfunction()
+}
+
+func mycustomfunction() {
+    fmt.Println("I am inside mycustomfunction")
+}
