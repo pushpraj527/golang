2838964d9e84e1e1903c5b503fa410c08ab61ca8
Pushp Raj <raj.pushp05@gmail.com>
2020-06-26 12:12:58 +0530

    packageintro example

---
+++ src/packageintro.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    
+	n, err := fmt.Println("Hello Raj", 27, true) // n and err is return by Println
+	fmt.Println(n,err)
+
+	//n, _ := fmt.Println("Hello Raj", 27, true) //here "_" is use to take void input
+	//fmt.Println(n)
+
+}
