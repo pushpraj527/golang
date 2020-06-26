addc7d9b1be33ed626c9448f9f044f18077a8dc4
Pushp Raj <raj.pushp05@gmail.com>
2020-06-26 12:28:35 +0530

    packageintro example1

--- src/packageintro.go
+++ src/packageintro.go
@@ -9,7 +9,7 @@ func main() {
 	n, err := fmt.Println("Hello Raj", 27, true) // n and err is return by Println
 	fmt.Println(n,err)
 
-	//n, _ := fmt.Println("Hello Raj", 27, true) //here "_" is use to take void input
-	//fmt.Println(n)
+	n, _ = fmt.Println("Hello Raj", 27, true) //here "_" is use to take void input
+	fmt.Println(n)
 
 }
